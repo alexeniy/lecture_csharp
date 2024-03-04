5e782ca6bbc28b44ed6c0d6b8611654a16ee0b11
Aleksei Korostelev <lehako95@gmail.com>
2024-03-04 01:55:34 +0100

    Added_task_04_Giri

---
+++ task_04/Program.cs
@@ -0,0 +1,33 @@
+﻿int a = 123;
+int b = 622;
+int c = 23;
+int d = 464;
+int e = 231;
+int f = 198;
+
+int max = a; 
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+if (f > max)
+{
+    max = f;
+}
+
+Console.WriteLine(max);
+
+
