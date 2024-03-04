07ca144cb6d362fd58a3aec682b66c6d338f8904
Aleksei Korostelev <lehako95@gmail.com>
2024-03-04 20:44:34 +0100

    Added_task_06

---
+++ task_06/Program.cs
@@ -0,0 +1,12 @@
+﻿long a = 763225;
+long b = 2345909;
+long c = 613245;
+long d = 7234563456467567;
+long value = a * b;
+long sum = c + d;
+
+double quotient = value / sum;
+
+Console.WriteLine(quotient);
+
+
