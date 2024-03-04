9e1ccc2492eafc2310f13a5def46c5cf96451f7e
Aleksei Korostelev <lehako95@gmail.com>
2024-03-04 01:20:52 +0100

    Task_01_Added_variables_1

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Helllooo, World!!!");
+﻿//Console.WriteLine("Helllooo, World!!!");
+
+using System.IO.Compression;
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
